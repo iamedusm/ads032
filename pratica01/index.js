fd31fc6d6a6e8f8dbcdc2b2ee5dd5a3bb9aeac08
unknown <2214290063@ACADEMICO.IESB.BR>
2023-04-04 21:55:09 -0300

    Criação Pratica 1

---
+++ pratica01/index.js
@@ -0,0 +1,10 @@
+const contatos = {
+    nome: "NOME DO CONTATO",
+    fone: "FONE DO CONTATO"
+}
+
+const root = document.getElementById("root")
+
+function titulo(nome) {
+    return `<h2>${nome}</h2>`
+}
